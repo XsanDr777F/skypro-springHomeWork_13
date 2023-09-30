5b64a6e83add4b9c426eaa17a7d6e8f6368881c2
Александр <79691189430@yandex.ru>
2023-09-30 21:18:08 +0300

    calculator works

---
+++ src/main/java/pro/sky/skypro/calculator/servise/CalculatorServise.java
@@ -0,0 +1,11 @@
+package pro.sky.skypro.calculator.servise;
+
+public interface CalculatorServise {
+    int sum(int num1, int num2);
+
+    int subtract(int num1, int num2);
+
+    int multiply(int num1, int num2);
+
+    int divide(int num1, int num2);
+}
